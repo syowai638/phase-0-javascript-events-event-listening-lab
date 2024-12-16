f8dc2ee548cac74ebadc627e75ef7122704cadaf
syowai638 <cmuthini77@gmail.com>
2024-12-16 19:35:57 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// index.js
+
 function addingEventListener() {
-}
+    const input = document.getElementById('button');
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add event listener to the button
+    input.addEventListener('click', clickAlert);
+  }
+  
+  // Call the function to add the event listener
+  addingEventListener();
+  
